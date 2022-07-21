a0e51a55e2885b45c9568dff9b2f4e1b62fa2a4e
fhidesjoseph <fhidesjoseph@gmail.com>
2022-07-22 00:09:41 +0300

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,24 @@
-import React from "react";
-import CategoryFilter from "./CategoryFilter";
+import React,{useState}from "react";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
+import CategoryFilter from "./CategoryFilter";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+//console.log("Here's the data you're working with");
+//console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [list, setList]=useState(TASKS);
+
+  const updateTasks=(newTask)=>{
+    setList([...list,newTask])
+  }
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={CATEGORIES}/>
+      <NewTaskForm categories={CATEGORIES} onAddTask={updateTasks} />
+      <TaskList tasks={list} />
     </div>
   );
 }
